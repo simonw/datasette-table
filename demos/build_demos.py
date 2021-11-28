2a34c37ef61042e444bad10a32f3e294cf80d0e7
Simon Willison <swillison@gmail.com>
2021-11-28 11:52:07 -0800

    Initial interactive demos, refs #1

---
+++ demos/build_demos.py
@@ -0,0 +1,42 @@
+import pathlib
+import re
+import subprocess
+
+
+index_re = re.compile(r"<!\-\- list-starts \-\->.*<!\-\- list-ends \-\->", re.DOTALL)
+
+dir = pathlib.Path(__file__).parent
+
+
+def get_tags():
+    output = subprocess.check_output(["git", "tag"]).decode("utf-8")
+    return [
+        l.strip()
+        for l in output.split("\n")
+        if l.strip()
+    ]
+
+
+def build_list(tags):
+    lines = ["<!-- list-starts -->"]
+    lines.extend(
+        '<li><a href="{tag}.html">{tag}</a></li>'.format(tag=tag) for tag in tags
+    )
+    lines.append("<!-- list-ends -->")
+    return "\n".join(lines)
+
+
+if __name__ == "__main__":
+    tags = get_tags()
+    index = dir / "index.html"
+    index_contents = index.open().read()
+    rewritten = index_re.sub(build_list(tags), index_contents)
+    index.write_text(rewritten, "utf-8")
+    # Write out a file per version
+    template = (dir / "_template.html").read_text()
+    for tag in tags:
+        content = template.replace(
+            "{version}", tag
+        )
+        version_file = dir / "{}.html".format(tag)
+        version_file.write_text(content, "utf-8")
